a1b5294607a15b6dbf2d229b24390c9524c87ba5
Nirmani Gunathilaka <nirmani7678@gmail.com>
2021-10-14 13:25:16 +0530

    backend first commit

---
+++ backend/src/test/java/com/nds/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nds.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
