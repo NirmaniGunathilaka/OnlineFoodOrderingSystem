a0fcd3f9f16fbb24e3a7b1e21a911445ca2c590d
Suhadini Kaluarachchi <kasuhadini@gmail.com>
2021-10-20 22:56:40 +0530

    Added sidebar to the admin dashboard

---
+++ frontend/src/components/Deals.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+export default function Deals() {
+    return (
+        <Box mt={5}>
+            <Typography variant="h5">Deals Here!</Typography>
+        </Box>
+    );
+}
