28cdd8e837d9f70b9a3b29f49f813695abbbb09f
dimuthi tharaka <dimuthi@gmail.com>
2021-10-29 13:01:45 +0530

    add new item form created

--- frontend/src/components/CustomerList.js
+++ frontend/src/components/CustomerList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -16,6 +16,7 @@ import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import LastPageIcon from '@mui/icons-material/LastPage';
 import IconButton from '@mui/material/IconButton';
 import TableFooter from '@mui/material/TableFooter';
+import SearchBar from "material-ui-search-bar";
 
 function TablePaginationActions(props) {
   const theme = useTheme();
@@ -98,21 +99,38 @@ function createData(id, name, email, address, contactNo) {
   return { id, name, email, address, contactNo };
 }
 
-const rows = [
+const originalRows = [
   createData(1, 'Amith Perera', 'amith@gmail.com', 'Colombo', '0765567880'),
   createData(2, 'Damith Asanka', 'damith@gmail.com', 'Matara', '0712234560'),
   createData(3, 'Kusal Mendis', 'kusal@gmail.com', 'Piliyandala', '0776678990'),
   createData(4, 'Sanka Dineth', 'sankad@gmail.com', 'Moratuwa', '0763345232'),
   createData(5, 'Nadeemal', 'nadeemal@gmail.com', 'Galle', '0783410080'),
   createData(6, 'Kasuni Gamage', 'kasuni@gmail.com', 'Kandy', '0713420080'),
-  createData(5, 'Ameya Thembe', 'ameya@gmail.com', 'Pune', '0943412080'),
-  createData(6, 'Charith Giragama', 'Charith@gmail.com', 'Colombo', '0713429980'),
+  createData(7, 'Ameya Thembe', 'ameya@gmail.com', 'Pune', '0943412080'),
+  createData(8, 'Charith Giragama', 'Charith@gmail.com', 'Colombo', '0713429980'),
 ];
 
 export default function CustomerList() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rows, setRows] = useState(originalRows);
+  const [searched, setSearched] = useState("");
 
+  const requestSearch = (searchedVal) => {
+    const filteredRows = originalRows.filter((row) => {
+      var result = row.name.toLowerCase().includes(searchedVal.toLowerCase()) ||
+        row.email.toLowerCase().includes(searchedVal.toLowerCase()) ||
+        row.address.toLowerCase().includes(searchedVal.toLowerCase()) ||
+        row.contactNo.toLowerCase().includes(searchedVal.toLowerCase());
+      return result;
+    });
+    setRows(filteredRows);
+  };
+
+  const cancelSearch = () => {
+    setSearched("");
+    requestSearch(searched);
+  };
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
@@ -126,62 +144,72 @@ export default function CustomerList() {
     setPage(0);
   };
   return (
-    <TableContainer component={Paper} sx={{maxWidth: "60%", margin: "auto"}}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Id</StyledTableCell>
-            <StyledTableCell align="left">Name</StyledTableCell>
-            <StyledTableCell align="left">Email</StyledTableCell>
-            <StyledTableCell align="left">Address</StyledTableCell>
-            <StyledTableCell align="left">Contact No.</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row) => (
-            <StyledTableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <StyledTableCell component="th" scope="row">
-                {row.id}
-              </StyledTableCell>
-              <StyledTableCell align="left">{row.name}</StyledTableCell>
-              <StyledTableCell align="left">{row.email}</StyledTableCell>
-              <StyledTableCell align="left">{row.address}</StyledTableCell>
-              <StyledTableCell align="left">{row.contactNo}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
+    <>
+      <SearchBar
+        autofocus
+        placeholder='Search Customers'
+        style={{ maxWidth: 300, marginLeft: '60%', marginBottom: '30px' }}
+        value={searched}
+        onChange={(searchVal) => requestSearch(searchVal)}
+        onCancelSearch={() => cancelSearch()}
+      />
+      <TableContainer component={Paper} sx={{ maxWidth: "80%", margin: "auto" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Id</StyledTableCell>
+              <StyledTableCell align="left">Name</StyledTableCell>
+              <StyledTableCell align="left">Email</StyledTableCell>
+              <StyledTableCell align="left">Address</StyledTableCell>
+              <StyledTableCell align="left">Contact No.</StyledTableCell>
             </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={5}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
+              <StyledTableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <StyledTableCell component="th" scope="row">
+                  {row.id}
+                </StyledTableCell>
+                <StyledTableCell align="left">{row.name}</StyledTableCell>
+                <StyledTableCell align="left">{row.email}</StyledTableCell>
+                <StyledTableCell align="left">{row.address}</StyledTableCell>
+                <StyledTableCell align="left">{row.contactNo}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={5}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    'aria-label': 'rows per page',
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
--- frontend/src/components/FoodItems.js
+++ frontend/src/components/FoodItems.js
@@ -9,6 +9,7 @@
 //     );
 // }
 import * as React from 'react';
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -19,6 +20,10 @@ import { Grid } from '@material-ui/core';
 import "../styles.css";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Popup from "./Popup";
+import ItemForm from "./ItemForm";
+
 
 import history from "./history";
 
@@ -30,155 +35,169 @@ const useStyles = makeStyles({
 });
 
 export default function FoodItems() {
+    const [openPopup, setOpenPopup] = useState(false)
+    const [recordForEdit, setRecordForEdit] = useState(null)
     const classes = useStyles();
     return (
-        <Grid
-            container
-            spacing={4}
-            className={classes.gridContainer}
-            justify="center"
-        >
-            <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
-                <Card sx={{ maxWidth: 345 }} style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/images/Piz.jpg"
-                        alt="pizza"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Pizza
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Pizza is an Italian dish consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature
-                        </Typography>
-                    </CardContent>
-                    <CardActions style={{ justifyContent: 'center' }}>
-                        <Button onClick={() => history.push("/pizza")} align="center" size="small">View Catalog</Button>
+        <Grid>
+            <Box m={2} pb={3}>
+                <Button variant="contained" style={{ backgroundColor: '#f58311' }} onClick={() => { setOpenPopup(true); setRecordForEdit(null); }}>Add New Item</Button>
 
-                    </CardActions>
-                </Card>
+            </Box>
+            <Popup
+                title="Employee Form"
+                openPopup={openPopup}
+                setOpenPopup={setOpenPopup}>
+                    <ItemForm/>
+            </Popup>
+            <Grid
+                container
+                spacing={4}
+                className={classes.gridContainer}
+                justify="center"
+            >
 
+                <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
+                    <Card sx={{ maxWidth: 345 }} style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/images/Piz.jpg"
+                            alt="pizza"
+                        />
+                        <CardContent sx={{ flexGrow: 2 }}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Pizza
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Pizza is an Italian dish consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature
+                            </Typography>
+                        </CardContent>
+                        <CardActions style={{ justifyContent: 'center' }}>
+                            <Button onClick={() => history.push("/pizza")} align="center" size="small">View Catalog</Button>
+
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
+                    <Card sx={{ maxWidth: 345, display: 'flex', flexDirection: 'column' }} >
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/images/pasta.jpg"
+                            alt="Pasta"
+                        />
+                        <CardContent sx={{ flexGrow: 2 }}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Pasta
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Simple, classic pasta recipes are always a crowd pleaser and naturally budget friendly
+                            </Typography>
+                        </CardContent>
+                        <CardActions style={{ justifyContent: 'center' }}>
+                            <Button align="center" size="small">View Catalog</Button>
+
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
+                    <Card sx={{ maxWidth: 345, display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/images/burger.jpg"
+                            alt="Burger"
+                        />
+                        <CardContent sx={{ flexGrow: 2 }}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Burger
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                A hamburger is a food, typically considered a sandwich, consisting of one or more cooked patties
+                            </Typography>
+                        </CardContent>
+                        <CardActions style={{ justifyContent: 'center' }}>
+                            <Button align="center" size="small">View catalog</Button>
+
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
+                    <Card sx={{ maxWidth: 345, display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/images/submarine.jpg"
+                            alt="submarine"
+                        />
+                        <CardContent sx={{ flexGrow: 2 }}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Submarine
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                A submarine sandwich, commonly known as a sub, hoagie, hero, Italian sandwich or grinder, is a type of cold or hot sandwich made from a cylindrical bread roll split lengthwise and filled with meats, cheeses, vegetables, and condiments
+                            </Typography>
+                        </CardContent>
+                        <CardActions style={{ justifyContent: 'center' }}>
+                            <Button align="center" size="small">view catalog</Button>
+
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
+                    <Card sx={{ maxWidth: 345, display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/images/noodles.jpg"
+                            alt="noodles"
+                        />
+                        <CardContent sx={{ flexGrow: 2 }}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Noodles
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Noodles are a type of food made from unleavened dough which is rolled flat and cut, stretched or extruded, into long strips or strings
+                            </Typography>
+                        </CardContent>
+                        <CardActions style={{ justifyContent: 'center' }}>
+                            <Button align="center" size="small">view catalog</Button>
+
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
+
+                    <Card sx={{ maxWidth: 345, display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/images/rice.jpg"
+                            alt="rice"
+                        />
+                        <CardContent sx={{ flexGrow: 2 }}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Rice
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat
+                            </Typography>
+                        </CardContent>
+                        <CardActions style={{ justifyContent: 'center' }} >
+                            <Button align="center" size="small">view catalog</Button>
+
+                        </CardActions>
+                    </Card>
+
+
+                </Grid >
             </Grid>
-            <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
-                <Card sx={{ maxWidth: 345 }} >
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/images/pasta.jpg"
-                        alt="Pasta"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Pasta
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Simple, classic pasta recipes are always a crowd pleaser and naturally budget friendly
-                        </Typography>
-                    </CardContent>
-                    <CardActions style={{ justifyContent: 'center', align: 'center', display: 'flex', alignSelf: 'flex-end' }}>
-                        <Button align="center" size="small">View Catalog</Button>
-
-                    </CardActions>
-                </Card>
-
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/images/burger.jpg"
-                        alt="Burger"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Burger
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            A hamburger is a food, typically considered a sandwich, consisting of one or more cooked patties
-                        </Typography>
-                    </CardContent>
-                    <CardActions style={{ justifyContent: 'center' }}>
-                        <Button align="center" size="small">View catalog</Button>
-
-                    </CardActions>
-                </Card>
-
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/images/submarine.jpg"
-                        alt="submarine"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Submarine
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            A submarine sandwich, commonly known as a sub, hoagie, hero, Italian sandwich or grinder, is a type of cold or hot sandwich made from a cylindrical bread roll split lengthwise and filled with meats, cheeses, vegetables, and condiments
-                        </Typography>
-                    </CardContent>
-                    <CardActions style={{ justifyContent: 'center' }}>
-                        <Button align="center" size="small">view catalog</Button>
-
-                    </CardActions>
-                </Card>
-
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/images/noodles.jpg"
-                        alt="noodles"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Noodles
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Noodles are a type of food made from unleavened dough which is rolled flat and cut, stretched or extruded, into long strips or strings
-                        </Typography>
-                    </CardContent>
-                    <CardActions style={{ justifyContent: 'center' }}>
-                        <Button align="center" size="small">view catalog</Button>
-
-                    </CardActions>
-                </Card>
-
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} item style={{ display: 'flex' }}>
-
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/images/rice.jpg"
-                        alt="rice"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Rice
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat
-                        </Typography>
-                    </CardContent>
-                    <CardActions style={{ justifyContent: 'center' }} >
-                        <Button  align="center" size="small">view catalog</Button>
-
-                    </CardActions>
-                </Card>
-
-
-            </Grid >
-
         </Grid >
 
     );
